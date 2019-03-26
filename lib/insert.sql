05a5cb1767f57c6fe6ec0b295da870ba6d486208
A Glazer <avigayilglazer@gmail.com>
2019-03-26 10:51:03 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+-- user table
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+INSERT INTO users (name, age) VALUES (NULL, NULL);
+
+-- projects table
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, NULL, NULL, NULL, NULL);
+
+-- pledges table
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (NULL, NULL, NULL);
