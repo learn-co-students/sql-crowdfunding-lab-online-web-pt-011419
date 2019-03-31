0eae3492988513c79f1f2f9c4c3c8f9a52bced14
Nissan Holzer <nissan.holzer@gmail.com>
2019-03-31 15:48:03 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'posting sheets', 'revenue', 1000.00, '2019-01-01', '2019-12-31')
+(2, 'google shopping', 'revenue', 10000.00, '2018-11-01', '2019-12-31')
+(3, 'reconciling credit card', 'backend', 500.00, '2019-01-01', '2019-12-15')
+(4, 'jet review', 'revenue', 250.00, '2019-05-01', '2019-06-30')
+(5, 'ebay review', 'revenue', 250.00, '2019-04-01', '2019-10-30')
+(6, 'customer service', 'revenue', 100000.00, '2019-01-01', '2019-12-31')
+(7, 'taxes', 'backend', 100.00, '2019-03-01', '2019-03-15')
+(8, 'returns', 'revnue', 50000.00, '2019-01-01', '2019-12-31')
+(9, 'coding', 'backend', 70000.00, '2019-01-01', '2019-12-31')
+(10, 'new products', 'revenue', 100000.00, '2019-01-01', '2019-12-31');
+
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17); 
+INSERT INTO users (id, name, age) VALUES (2, 'Bear', 6);
+INSERT INTO users (id, name, age) VALUES (3, 'Iguana', 4);
+INSERT INTO users (id, name, age) VALUES (4, 'Alex', 33);
+INSERT INTO users (id, name, age) VALUES (5, 'Amanda', 24);
+INSERT INTO users (id, name, age) VALUES (6, 'Sophie', 24); 
+INSERT INTO users (id, name, age) VALUES (7, 'Rosey', 9); 
+INSERT INTO users (id, name, age) VALUES (8, 'Victoria', 23); 
+INSERT INTO users (id, name, age) VALUES (9, 'Franz', 100);
+INSERT INTO users (id, name, age) VALUES (10, 'Hermione', 30);
+INSERT INTO users (id, name, age) VALUES (11, 'Voldemort', 90); 
+(12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
