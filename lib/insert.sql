8d0718f645950fd75a6e50b52272a0a4c9af55b2
Nissan Holzer <nissan.holzer@gmail.com>
2019-03-31 15:48:57 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -21,5 +21,12 @@ INSERT INTO users (id, name, age) VALUES (8, 'Victoria', 23);
 INSERT INTO users (id, name, age) VALUES (9, 'Franz', 100);
 INSERT INTO users (id, name, age) VALUES (10, 'Hermione', 30);
 INSERT INTO users (id, name, age) VALUES (11, 'Voldemort', 90); 
-(12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
-(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
+INSERT INTO users (id, name, age) VALUES (12, 'Marisa', 24); 
+INSERT INTO users (id, name, age) VALUES (13, 'Swizzle', 4); 
+INSERT INTO users (id, name, age) VALUES (14, 'Sirius', 36); 
+INSERT INTO users (id, name, age) VALUES (15, 'Albus', 113); 
+INSERT INTO users (id, name, age) VALUES (16, 'Squid', 5);
+INSERT INTO users (id, name, age) VALUES (17, 'Whale', 6); 
+INSERT INTO users (id, name, age) VALUES (18, 'Pacha', 5); 
+INSERT INTO users (id, name, age) VALUES (19, 'Ena', 24); 
+INSERT INTO users (id, name, age) VALUES (20, 'Katie', 24);
